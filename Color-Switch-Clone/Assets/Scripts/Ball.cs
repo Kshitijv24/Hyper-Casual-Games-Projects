d0565a7a91c164dbf3b959f00e973269824ae87f
Kshitijv24 <kshitijv24@gmail.com>
2022-12-17 10:51:33 +0530

    implemented the logic in which if the player touch the change change color game object then the player ball color change to that color and it's tag is set to the that color so if both of these things are the same then it can go through the small circle if the ball matches the color and tag

--- Color-Switch-Clone/Assets/Scripts/Ball.cs
+++ Color-Switch-Clone/Assets/Scripts/Ball.cs
@@ -6,7 +6,6 @@ public class Ball : MonoBehaviour
     [SerializeField] int moveAmount;
 
     Rigidbody2D rb;
-    Vector2 direction;
 
     private void Start()
     {
@@ -24,13 +23,5 @@ public class Ball : MonoBehaviour
     private void MoveBallUp()
     {
         rb.velocity = Vector2.up * moveAmount;
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.tag == "Blue")
-        {
-            collision.gameObject.GetComponent<PolygonCollider2D>().isTrigger = true;
-        }
     }
 }
--- Color-Switch-Clone/Assets/Scripts/ColorChangeBall.cs
+++ Color-Switch-Clone/Assets/Scripts/ColorChangeBall.cs
@@ -3,18 +3,44 @@ using UnityEngine;
 public class ColorChangeBall : MonoBehaviour
 {
     [SerializeField] Color[] colors;
+    [SerializeField] SpriteRenderer ballSpriteRenderer;
 
     SpriteRenderer spriteRenderer;
+    Color currentColor;
 
     private void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
-        spriteRenderer.color = colors[Random.Range(0, colors.Length)];
+        currentColor = colors[Random.Range(0, colors.Length)];
+        spriteRenderer.color = currentColor;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        collision.gameObject.GetComponent<SpriteRenderer>().color = spriteRenderer.color;
-        //collision.gameObject.tag = 
+        SwitchBallTagAndColor(collision);
+    }
+
+    private void SwitchBallTagAndColor(Collider2D collision)
+    {
+        if(currentColor == colors[0])
+        {
+            collision.gameObject.tag = "Blue";
+            ballSpriteRenderer.color = colors[0];
+        }
+        else if (currentColor == colors[1])
+        {
+            collision.gameObject.tag = "Yellow";
+            ballSpriteRenderer.color = colors[1];
+        }
+        else if (currentColor == colors[2])
+        {
+            collision.gameObject.tag = "Pink";
+            ballSpriteRenderer.color = colors[2];
+        }
+        else if (currentColor == colors[3])
+        {
+            collision.gameObject.tag = "Purple";
+            ballSpriteRenderer.color = colors[3];
+        }
     }
 }
