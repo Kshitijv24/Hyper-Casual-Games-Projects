64ceb4e0a7a644a0d1a844c8196910dcbd02b0bf
Kshitijv24 <kshitijv24@gmail.com>
2022-12-27 12:38:59 +0530

    Bubble Struggle Game Project setup completed

---
+++ Bubble Struggle/Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (Touchscreen.current.primaryTouch.press.isPressed)
+        {
+
+        }
+    }
+}
